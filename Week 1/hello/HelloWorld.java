e6d9b64ae76105415061f4451a80152b57e168b2
Matthew Widjaja <Matthew@MBRetina.local>
2014-07-03 21:08:34 -0400

    Week 1
    Class Content only: Hello World & Square Apple

---
+++ Week 1/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * A simple program to send a greeting to the world
+ * 
+ */
+public class HelloWorld
+{
+   public static void main(String[] args) {
+       
+       System.out.println("Hello, world!");
+       System.out.println("Hello, Matt!");
+       System.out.println("全世界，大家好");
+   }
+}
