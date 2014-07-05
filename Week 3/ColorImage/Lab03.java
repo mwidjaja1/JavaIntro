63c660ecaa774bd60187d3cb9f5ca67becffb1cd
Matthew Widjaja <Matthew@MBRetina.local>
2014-07-04 23:17:43 -0400

    3: ColorImage (Done) & Football (Original)
    ColorImage can now display one image (A.jpg), two images (B.jpg), or 3
    images (A.jpg, B.jpg, and an added or multiplied version).
    
    The football script will be able to print a flag over a soccer ball.

--- Week 3/ColorImage/Lab03.java
+++ Week 3/ColorImage/Lab03.java
@@ -7,25 +7,37 @@ public class Lab03
     public void loadAnImage() 
     {
         // Please write your code after this line
-        
-        
-        
+        ColorImage image = new ColorImage();
+        Canvas canvas = new Canvas(image.getWidth(), image.getHeight());
+        canvas.add(image);
         
     }
     
     public void loadTwoImagesOnTheSameCanvas()
     {
         // Please write your code after this line
-        
-        
-        
+        ColorImage image1 = new ColorImage();
+        ColorImage image2 = new ColorImage();
+        int x = image1.getWidth()*2;
+        int y = image1.getHeight();
+        Canvas canvas = new Canvas(x, y);
+        canvas.add(image1);
+        canvas.add(image2, y, 0);
         
     }
     
     public void imageAddition() 
     {    
         // Please write your code after this line
-        
+        ColorImage image1 = new ColorImage();
+        ColorImage image2 = new ColorImage();
+        ColorImage image3 = ColorImage.add(image1,image2);
+        int x = image1.getWidth()*3;
+        int y = image1.getHeight();
+        Canvas canvas = new Canvas(x+20, y);
+        canvas.add(image1);
+        canvas.add(image2, y+10, 0);
+        canvas.add(image3, y*2+20, 0);
         
         
         
@@ -34,7 +46,15 @@ public class Lab03
     public void imageMultiplication() 
     {
         // Please write your code after this line
-        
+        ColorImage image1 = new ColorImage();
+        ColorImage image2 = new ColorImage();
+        ColorImage image3 = ColorImage.multiply(image1,image2);
+        int x = image1.getWidth()*3;
+        int y = image1.getHeight();
+        Canvas canvas = new Canvas(x+20, y);
+        canvas.add(image1);
+        canvas.add(image2, y+10, 0);
+        canvas.add(image3, y*2+20, 0);
         
         
         
